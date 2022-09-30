b56970d4b80bc9ab27e1416348d908c08432e9da
Danielle <dmbecker24@gmail.com>
2022-09-29 18:03:40 -0700

    Complete function array_to_bst. Pass all tests

--- binary_search_trees/array_to_bst.py
+++ binary_search_trees/array_to_bst.py
@@ -4,10 +4,18 @@ class TreeNode:
         self.left = left
         self.right = right
 
+def find_middle(arr):
+    list_length = len(arr)
+    mid_index = list_length // 2
+    return mid_index
+
 
 def arr_to_bst(arr):
-    """ Given a sorted array, write a function to create a 
-        Balanced Binary Search Tree using the elements in the array.
-        Return the root of the Binary Search Tree.
-    """
-    pass
+    if not arr:
+        return None
+    mid_index = find_middle(arr)
+    root = TreeNode(arr[mid_index])
+
+    root.left = arr_to_bst(arr[:mid_index])
+    root.right = arr_to_bst(arr[mid_index + 1:])
+    return root
