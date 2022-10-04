a9bc9bc625420f961c003a8359ff437d603f5c4c
Georgia Adam <georgiaeadam@gmail.com>
2022-10-03 22:36:46 -0400

    tests passing

--- binary_search_trees/array_to_bst.py
+++ binary_search_trees/array_to_bst.py
@@ -10,4 +10,14 @@ def arr_to_bst(arr):
         Balanced Binary Search Tree using the elements in the array.
         Return the root of the Binary Search Tree.
     """
-    pass
+    def convert(arr, l, r):
+        if l > r:
+            return None
+    
+        m = l + (r - l) // 2
+        root = TreeNode(arr[m])
+        root.left = convert(arr, l, m - 1)
+        root.right = convert(arr, m + 1, r)
+        return root
+
+    return convert(arr, 0, len(arr) - 1)
