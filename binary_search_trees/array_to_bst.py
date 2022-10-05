a13a11292ba56d7ad67657d6e00037a1154223ca
Gabriela Ibarra <gabriela.estefania.ibarra@gmail.com>
2022-10-05 17:55:20 -0500

    Complete arr_to_bst function. Passes all unit tests

--- binary_search_trees/array_to_bst.py
+++ binary_search_trees/array_to_bst.py
@@ -10,25 +10,12 @@ def arr_to_bst(arr):
         Balanced Binary Search Tree using the elements in the array.
         Return the root of the Binary Search Tree.
     """
-    '''
-    1. Recursivley assign root to be whatever you get returned for left/right side of the list. 
-    2. Function takes in list. 
-        a. Assign left to be middle of left list. 
-        b. Assign right to middle of right list. 
-        root.left = arr_to_bst(left_side)
-        root.right = arr_to_bst(right_side)
-        return root
-    '''
     if not arr:
         return None
     if len(arr) == 1:
         return TreeNode(arr[0])
     mid = len(arr) // 2
     root = TreeNode(arr[mid])
-    # if not root.left: # May not need this
-        # Call function again, but only with left side of list
-    root.left = arr_to_bst(arr[:mid]) # Assign root.left and root.right to be these values
-    # if not root.right:
-    root.right = arr_to_bst(arr[mid + 1:]) # Only want to grab mid + 1
-    # Consider returning something at the end (root of tree)
+    root.left = arr_to_bst(arr[:mid]) 
+    root.right = arr_to_bst(arr[mid + 1:]) 
     return root
