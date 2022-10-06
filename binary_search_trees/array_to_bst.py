b1f223ba57d87ba30edd35a1ba22cce45a6adde1
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-10-06 11:55:47 -0700

    passes 4/4 tests

--- binary_search_trees/array_to_bst.py
+++ binary_search_trees/array_to_bst.py
@@ -10,4 +10,20 @@ def arr_to_bst(arr):
         Balanced Binary Search Tree using the elements in the array.
         Return the root of the Binary Search Tree.
     """
-    pass
+    # If array is empty
+    if not arr:
+        return None
+    
+    # Find the middle element and make it the root node
+    # Want to repeat this for the left and right subtrees
+    mid_ind = (len(arr)) // 2
+    root = TreeNode(arr[mid_ind])
+
+    # Left subtree
+    root.left = arr_to_bst(arr[:mid_ind])
+
+    # Right subtree
+    # +1 so we don't include the current mid in the next call
+    root.right = arr_to_bst(arr[mid_ind + 1:])
+
+    return root
