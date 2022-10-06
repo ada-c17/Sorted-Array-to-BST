9eb3554b49a1fe9dd7487e47dc448487882b9d8c
shelby-faulconer <shelby.faulconer@disneystreaming.com>
2022-10-06 13:30:30 -0700

    added more pseudo code

--- binary_search_trees/array_to_bst.py
+++ binary_search_trees/array_to_bst.py
@@ -10,4 +10,11 @@ def arr_to_bst(arr):
         Balanced Binary Search Tree using the elements in the array.
         Return the root of the Binary Search Tree.
     """
+    ## recommended to make problem recursive starting at the middle of the list
+    # create a root node with the middle element in the list
+    # check the second element in the list
+    # if that element is larger or equal to the root:
+    # that element becomes the left child node
+    # if that element is smaller or equal to the root:
+    # that element becomes the right child node
     pass
