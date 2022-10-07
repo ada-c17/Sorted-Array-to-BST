35134f5dab7227435e7b226fa248b60e65fa682b
Megan Korling <korlingm@gmail.com>
2022-10-06 17:58:37 -0700

    working solution passes all tests

--- binary_search_trees/array_to_bst.py
+++ binary_search_trees/array_to_bst.py
@@ -5,9 +5,25 @@ class TreeNode:
         self.right = right
 
 
+# arr = [5, 10, 15, 20, 25, 30, 35, 40, 45]
+
 def arr_to_bst(arr):
     """ Given a sorted array, write a function to create a 
         Balanced Binary Search Tree using the elements in the array.
         Return the root of the Binary Search Tree.
     """
-    pass
+    if not arr:
+        return None
+    # get the middle index, set as root
+    mid_index = len(arr)//2
+    root = TreeNode(arr[mid_index])
+    # while there is something to the right of the index
+        # set nodes for the right
+    root.left = arr_to_bst(arr[:mid_index])
+    
+    # while there is something to the right of the index
+        # set nodes for the left
+    root.right = arr_to_bst(arr[mid_index+1:])
+
+    return root
+    
