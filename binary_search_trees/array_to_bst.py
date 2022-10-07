404215e2efabff812342c94e33250d4f47bc0518
Jodi Denney <iarrived@gmail.com>
2022-10-06 17:08:01 -0700

    pass all tests

--- binary_search_trees/array_to_bst.py
+++ binary_search_trees/array_to_bst.py
@@ -1,13 +1,27 @@
 class TreeNode:
-    def __init__(self, value, left = None, right = None):
+    def __init__(self, value, left=None, right=None):
         self.val = value
         self.left = left
         self.right = right
 
 
+class Tree:
+    def __init__(self, root):
+        self.root = root
+
+
 def arr_to_bst(arr):
-    """ Given a sorted array, write a function to create a 
+    """ Given a sorted array, write a function to create a
         Balanced Binary Search Tree using the elements in the array.
         Return the root of the Binary Search Tree.
     """
-    pass
+    if not arr:
+        return None
+
+    mid_index = len(arr)//2
+    midpoint = arr[mid_index]
+    node = TreeNode(midpoint)
+    node.left = arr_to_bst(arr[0:mid_index])
+    node.right = arr_to_bst(arr[mid_index + 1:])
+
+    return node
