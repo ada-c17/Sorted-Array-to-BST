fd3ad5b049608023ff3036c463f8827625e7b039
Nina <ninagetsbored@gmail.com>
2022-10-08 21:30:06 -0700

    passes all 4 tests

--- binary_search_trees/array_to_bst.py
+++ binary_search_trees/array_to_bst.py
@@ -6,8 +6,21 @@ class TreeNode:
 
 
 def arr_to_bst(arr):
-    """ Given a sorted array, write a function to create a 
+    """ Given a sorted array, write a function to create a
         Balanced Binary Search Tree using the elements in the array.
         Return the root of the Binary Search Tree.
     """
-    pass
+    if not arr:
+        return None
+
+    # define root, which is median of arr
+    median = int(len(arr)/ 2)
+    root = TreeNode(arr[median])
+
+    # left subtree
+    root.left = arr_to_bst(arr[:median])
+
+    # right subtree
+    root.right = arr_to_bst(arr[median + 1:])
+
+    return root
