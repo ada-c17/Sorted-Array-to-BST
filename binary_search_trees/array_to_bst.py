48af0dc388a13310eb3238a9878c1259a25396ae
Sreymompech <sanchhen2013@gmail.com>
2022-10-08 21:53:40 -0700

    implementing of code

--- binary_search_trees/array_to_bst.py
+++ binary_search_trees/array_to_bst.py
@@ -10,4 +10,17 @@ def arr_to_bst(arr):
         Balanced Binary Search Tree using the elements in the array.
         Return the root of the Binary Search Tree.
     """
-    pass
+
+    # return None if array is empty
+    if len(arr) == 0:
+        return None
+    
+    # finding mid pointer
+    mid = len(arr) // 2
+    # creating root of Node
+    root = TreeNode(arr[mid])
+    # creating left subtree of tree recursively
+    root.left = arr_to_bst(arr[:mid])
+    # creating right subtree of tree recursively
+    root.right = arr_to_bst(arr[mid + 1:])
+    return root
