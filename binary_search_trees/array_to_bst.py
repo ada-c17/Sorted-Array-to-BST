a06b287fb975eb7a5fb03407eff513845efe01ce
NinaPatrina <ninochca@gmail.com>
2022-10-13 16:28:19 -0700

    project completed. all tests passed

--- binary_search_trees/array_to_bst.py
+++ binary_search_trees/array_to_bst.py
@@ -10,4 +10,14 @@ def arr_to_bst(arr):
         Balanced Binary Search Tree using the elements in the array.
         Return the root of the Binary Search Tree.
     """
-    pass
+
+    if not arr:
+        return None
+
+    mid_p = len(arr)//2
+    root=TreeNode(arr[mid_p])
+
+    root.left=arr_to_bst(arr[:mid_p])
+    root.right=arr_to_bst(arr[mid_p+1:])
+
+    return root
