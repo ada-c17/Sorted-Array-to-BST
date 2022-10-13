f12bf6a22af16d338c909d265bead090d09b949a
Jenny <jfperham@gmail.com>
2022-10-12 20:20:28 -0700

    BST assignment

--- binary_search_trees/array_to_bst.py
+++ binary_search_trees/array_to_bst.py
@@ -1,8 +1,18 @@
 class TreeNode:
-    def __init__(self, value, left = None, right = None):
+    def __init__(self, value, left=None, right=None):
         self.val = value
         self.left = left
         self.right = right
+
+    def assign_node(self, parent_node):
+        if self.value >= parent_node.value:
+            parent_node.right = self
+            parent_node = self
+            return
+        else:
+            parent_node.left = self
+            parent_node = self
+            return
 
 
 def arr_to_bst(arr):
@@ -10,4 +20,13 @@ def arr_to_bst(arr):
         Balanced Binary Search Tree using the elements in the array.
         Return the root of the Binary Search Tree.
     """
-    pass
+    if arr == []:
+        return None
+    mid_point = arr[len(arr)//2]
+    mid_index = len(arr)//2
+    root = TreeNode(mid_point)
+
+    root.left = arr_to_bst(arr[0:mid_index])
+    root.right = arr_to_bst(arr[mid_index + 1::])
+
+    return root
