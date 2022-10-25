99a33cd8b871fc87d51611cbda4e9cef780d66dd
Esther Annorzie <estherannorzie@gmail.com>
2022-10-25 15:30:00 -0400

    Complete project

--- binary_search_trees/array_to_bst.py
+++ binary_search_trees/array_to_bst.py
@@ -10,41 +10,15 @@ def arr_to_bst(arr):
         Balanced Binary Search Tree using the elements in the array.
         Return the root of the Binary Search Tree.
     """  
-    def add(self, current, value):
-        arr_length = len(arr)
-        midpoint = arr_length // 2
+    if not arr:
+        return None
 
-        # set the midpoint to the root of the BST
-        self.root = TreeNode(arr[midpoint])
+    arr_length = len(arr)
+    midpoint = arr_length // 2
 
-        # set the current to the root
-        current = self.root
+    root = TreeNode(arr[midpoint])
 
-        # loop from the end of arr until the beginning
-        # for index in range(len(arr) - 1, -1, -1):
-        for i in arr:
-            # if the midpoint is reached, skip it
-            # if index == midpoint:
-            if i == midpoint:
-                continue
+    root.left = arr_to_bst(arr[:midpoint])
+    root.right = arr_to_bst(arr[midpoint + 1:])
 
-            # else if arr[index] is less then the current node...
-            # elif arr[index] < current:
-            elif i < current:
-                # determine if the left subtree is None...
-                if current.left is None:
-                    # current.left = TreeNode(arr[index])
-                    current.left = TreeNode(i)
-                else:
-                    self.add(current.left, i)
-            # if the number is greater than or equal to the current node...
-            else:
-                # if the current node's right pointer is None...
-                if current.right is None:
-                    # set the right node to the number
-                    current.right = TreeNode(i)
-                # otherwise, recurse
-                else:
-                    self.add(current.right, i)
-            
-        return self.root   
+    return root
