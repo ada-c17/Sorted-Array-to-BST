74f033c72cd692812c74fe06540c902b1bbc31c6
Alexa Coffman <coffman.alexa@gmail.com>
2023-01-12 14:55:35 -0800

    Complete arr_to_bst method git push

--- binary_search_trees/array_to_bst.py
+++ binary_search_trees/array_to_bst.py
@@ -10,4 +10,13 @@ def arr_to_bst(arr):
         Balanced Binary Search Tree using the elements in the array.
         Return the root of the Binary Search Tree.
     """
-    pass
+    if not arr:
+        return None
+
+    mid = len(arr) // 2
+
+    return TreeNode(
+        arr[mid], 
+        arr_to_bst(arr[:mid]), 
+        arr_to_bst(arr[mid + 1:])
+        )
